d2e5ceb9e51ddee84c52b705d9830b7a5ab25951
kumarananda <coderanandakumar@gmail.com>
2022-04-24 09:12:18 +0600

    upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,148 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+
+
+<footer id="footer-widgets">
+      <div className="container">
+        <div className="go-top">
+          <a href="#top">
+            <i className="ti-arrow-up"></i>
+          </a>
+        </div>
+        <div className="row">
+          <div className="col-md-6 ov-h">
+            <div className="row">
+              <div className="col-sm-4">
+                <div className="widget">
+                  <h6 className="upper">About Comet</h6>
+                  <p>
+                    <span>Fourth Floor</span>
+                    <span>New York, NY 10011</span>
+                    <span>hello@comet.com</span>
+                  </p><a href="#" className="btn btn-color-out btn-sm">Hire Us!</a>
+                </div>
+              </div>
+              <div className="col-sm-4">
+                <div className="widget">
+                  <h6 className="upper">Culture</h6>
+                  <ul className="list-unstyled">
+                    <li>
+                      <a href="#">Team</a>
+                    </li>
+                    <li>
+                      <a href="#">Process</a>
+                    </li>
+                    <li>
+                      <a href="#">Story</a>
+                    </li>
+                    <li>
+                      <a href="#">Careers</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-sm-4">
+                <div className="widget">
+                  <h6 className="upper">Case Studies</h6>
+                  <ul className="list-unstyled">
+                    <li>
+                      <a href="portfolio-single-1.html">Neleman Cava</a>
+                    </li>
+                    <li>
+                      <a href="portfolio-single-2.html">Sweet Lane</a>
+                    </li>
+                    <li>
+                      <a href="portfolio-single-3.html">Jeff Burger</a>
+                    </li>
+                    <li>
+                      <a href="portfolio-single-1.html">Juice Meds</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 col-md-offset-2">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="widget">
+                  <h6 className="upper">Stay in touch</h6>
+                  <p>Sign Up to get the latest updates.</p>
+                  <div className="footer-newsletter">
+                    <form data-mailchimp="true" className="inline-form">
+                      <div className="input-group">
+                        <input type="email" name="email" placeholder="Your Email" className="form-control"/><span className="input-group-btn"><button type="submit" data-loading-text="Loading..." className="btn btn-color">Subscribe</button></span>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <!-- end of row--> */}
+      </div>
+      {/* <!-- end of container--> */}
+    </footer>
+
+    <footer id="footer">
+      <div className="container">
+        <div className="footer-wrap">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="copy-text">
+                <p><i className="icon-heart red mr-15"></i>© 2015 Comet Agency.</p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <ul className="list-inline">
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Blog</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-4">
+              <div className="footer-social">
+                <ul>
+                  <li>
+                    <a target="_blank" href="#"><i className="ti-facebook"></i></a>
+                  </li>
+                  <li>
+                    <a target="_blank" href="#"><i className="ti-twitter-alt"></i></a>
+                  </li>
+                  <li>
+                    <a target="_blank" href="#"><i className="ti-linkedin"></i></a>
+                  </li>
+                  <li>
+                    <a target="_blank" href="#"><i className="ti-instagram"></i></a>
+                  </li>
+                  <li>
+                    <a target="_blank" href="#"><i className="ti-dribbble"></i></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          {/* <!-- end of row--> */}
+        </div>
+      </div>
+      {/* <!-- end of container--> */}
+    </footer>
+
+    </>
+  )
+}
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,137 @@
+import React from 'react'
+import logo from '../../_assets/images/logo_light.png'
+
+const Header = () => {
+  return (
+    <>
+        {/* <!-- Navigation Bar--> */} 
+        <header id="topnav">
+            <div className="container">
+                {/* <!-- Logo container--> */} 
+                <div className="logo">
+                <a href="index-2.html">
+                    <img src={ logo } alt="" className="logo-light"/> 
+                    <img src="images/logo_dark.png" alt="" className="logo-dark"/>
+                </a>
+                </div>
+                {/* <!-- End Logo container--> */}
+                <div className="menu-extras">
+                <div className="menu-item">
+                    {/* <!-- Shopping cart--> */} 
+                    <div className="cart">
+                    <a href="#">
+                        <i className="ti-bag"></i><span className="cart-number">2</span>
+                    </a>
+                    <div className="shopping-cart">
+                        <div className="shopping-cart-info">
+                        <div className="row">
+                            <div className="col-xs-6">
+                            <h6 className="upper">Your Cart</h6>
+                            </div>
+                            <div className="col-xs-6 text-right">
+                            <h6 className="upper">$399.99</h6>
+                            </div>
+                        </div>
+                        {/* <!-- end of row--> */}
+                        </div>
+                        <ul className="nav product-list">
+                        <li>
+                            <div className="product-thumbnail">
+                            <img src="images/shop/2.jpg" alt=""/>
+                            </div>
+                            <div className="product-summary">
+                            <a href="#">Premium Suit Blazer</a><span>$199.99</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="product-thumbnail">
+                            <img src="images/shop/5.jpg" alt=""/>
+                            </div>
+                            <div className="product-summary">
+                            <a href="#">Reiss Vara Tailored Blazer</a><span>$199.99</span>
+                            </div>
+                        </li>
+                        </ul>
+                        <p><a href="#" className="btn btn-color btn-block btn-sm">Checkout</a>
+                        </p>
+                    </div>
+                    </div>
+                    {/* <!-- End shopping cart--> */}
+                </div>
+                <div className="menu-item">
+                    {/* <!-- Search Form--> */}
+                    <div className="search">
+                    <a href="#">
+                        <i className="ti-search"></i>
+                    </a>
+                    <div className="search-form">
+                        <form action="#" className="inline-form">
+                        <div className="input-group">
+                            <input type="text" placeholder="Search" className="form-control"/><span className="input-group-btn"><button type="button" className="btn btn-color"><span><i className="ti-search"></i></span>
+                            </button>
+                            </span>
+                        </div>
+                        </form>
+                    </div>
+                    </div>
+                    {/* <!-- End search form--> */}
+                </div>
+                <div className="menu-item">
+                    {/* <!-- Mobile menu toggle--> */}
+                    <a className="navbar-toggle">
+                    <div className="lines">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    </a>
+                    {/* <!-- End mobile menu toggle--> */}
+                </div>
+                </div>
+                <div id="navigation">
+                {/* <!-- Navigation Menu--> */}
+                <ul className="navigation-menu">
+                    <li className="has-submenu">
+                    <a href="#">Home</a>
+                    </li>
+                    <li className="has-submenu">
+                    <a href="#">blog-page </a>
+                    </li>
+                    <li className="has-submenu">
+                    <a href="#">pricing</a>
+                    </li>
+                    <li className="has-submenu">
+                    <a href="#">Contact</a>
+                    </li>
+
+                </ul>
+                    {/* <!-- End navigation menu        --> */}
+                </div>
+            </div>
+        </header>
+
+        <section className="page-title parallax">
+            <div data-parallax="scroll" data-image-src="images/bg/19.jpg" className="parallax-bg"></div>
+            <div className="parallax-overlay">
+                <div className="centrize">
+                <div className="v-center">
+                    <div className="container">
+                    <div className="title center">
+                        <h1 className="upper">Shop</h1>
+                        <h4>Free Delivery Worldwide.</h4>
+                        <hr/>
+                    </div>
+                    </div>
+                    {/* <!-- end of container--> */}
+                </div>
+                </div>
+            </div>
+        </section>
+    
+    
+    
+    </>
+  )
+}
+
+export default Header
