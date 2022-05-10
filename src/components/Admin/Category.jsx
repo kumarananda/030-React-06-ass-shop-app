ef96ca941049417d60f0ef8e97bb01cc34e65767
kumarananda <coderanandakumar@gmail.com>
2022-05-11 03:01:09 +0600

    edit com,

--- src/components/Admin/Category.jsx
+++ src/components/Admin/Category.jsx
@@ -1,11 +1,80 @@
 
-import React from 'react'
-import { Table, Button } from 'react-bootstrap'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { Table, Button, Form } from 'react-bootstrap'
 
 const Category = () => {
+
+    //slug Genarete
+    const makeSlug = (data) => {
+      let arr = data.split(' ');
+      return arr.join('-').toLowerCase();
+    }
+
+  // category Form state
+  const [categoryForm, setCategoryForm ] = useState(false);
+
+  // handle Add form
+  const handleAddform = () => {
+    setCategoryForm(true)
+  }
+
+  // input category data
+  const [category, setCategory] = useState({
+    name : '',
+    id : '',
+    slug : ''
+  });
+
+  // Category  add handle
+  const handleCatAddSubmit = (e) => {
+    e.preventDefault();
+
+    if(category.name === ''){
+      alert('empty')
+    }else {
+
+    let slug = makeSlug(category.name)
+    axios.post('http://localhost:5050/category', {
+      id : '',
+      name : category.name,
+      slug : slug
+    }).then(res => {
+      setCategory({
+        name : ''
+      })
+      setCategoryForm(false)
+    }).catch(err => {
+      console.log(err);
+    })
+    }
+
+  }  
+
+
+
+
   return (
     <>
       <h1>Category</h1>
+      
+      <Button onClick={handleAddform} className='btn btn-info btn-sm' >Add New tag</Button>
+      <hr />
+      {
+        categoryForm && 
+        <>
+        
+          <Form onSubmit={handleCatAddSubmit} >
+            <Form.Group className='my-3'>
+              <Form.Control value={category.name}  onChange={ e => setCategory({ ...category, name : e.target.value})}  type='text' placeholder='Category Name'/>
+            </Form.Group>
+            <br />
+            <Form.Group className=' my-3'>
+              <Button  type='submit' variant='success' className='btn-sm' >Add</Button>
+            </Form.Group>
+          </Form>
+        </>
+      }
       <hr />
       <Table>
         <thead>
